cdd0ae3bd8862f770e80f14dd17410cbe685fe9c
c_plus_c <dtm.music.producer@gmail.com>
2014-02-28 04:17:29 +0900

    レーダーの描画順変更

--- RosAssignment/frame.cpp
+++ RosAssignment/frame.cpp
@@ -261,14 +261,6 @@ void Frame::DrawRadar()
 {
 	DrawExtendGraph(601, 401, 601 + 184, 401 + 184, ResourceHandles::getResourceHandles().RadarPictureHandle, 1);
 
-	{
-		VECTOR ptrans = ObjectField::getObjectField().player.GetTranslation();
-		VECTOR pdir = ObjectField::getObjectField().player.GetDirection();
-
-		DrawRotaGraph(692 + 92 * (ptrans.x / ACTIVE_RADIUS), 493 - 92 * (ptrans.z / ACTIVE_RADIUS), 0.45, atan2(-pdir.z, pdir.x) + DX_PI / 2, ResourceHandles::getResourceHandles().playerIconHandle, 1);
-		DrawFormatString(692 + 92 * (ptrans.x / ACTIVE_RADIUS) - 25, 493 - 92 * (ptrans.z / ACTIVE_RADIUS) - 25, GetColor(255, 255, 255), "%d", (int) ptrans.y);
-	}
-
 	auto &enemies = ObjectField::getObjectField().Enemies;
 	for (auto ite = enemies.begin(); ite != enemies.end(); ++ite)
 	{
@@ -282,6 +274,14 @@ void Frame::DrawRadar()
 	{
 		VECTOR ptrans = ite->GetTranslation();
 		DrawRotaGraph(692 + 92 * (ptrans.x / ACTIVE_RADIUS), 493 - 92 * (ptrans.z / ACTIVE_RADIUS), 0.45, 0, ResourceHandles::getResourceHandles().BulletIconHandle, 1);
+		DrawFormatString(692 + 92 * (ptrans.x / ACTIVE_RADIUS) - 25, 493 - 92 * (ptrans.z / ACTIVE_RADIUS) - 25, GetColor(255, 255, 255), "%d", (int) ptrans.y);
+	}
+
+	{
+		VECTOR ptrans = ObjectField::getObjectField().player.GetTranslation();
+		VECTOR pdir = ObjectField::getObjectField().player.GetDirection();
+
+		DrawRotaGraph(692 + 92 * (ptrans.x / ACTIVE_RADIUS), 493 - 92 * (ptrans.z / ACTIVE_RADIUS), 0.45, atan2(-pdir.z, pdir.x) + DX_PI / 2, ResourceHandles::getResourceHandles().playerIconHandle, 1);
 		DrawFormatString(692 + 92 * (ptrans.x / ACTIVE_RADIUS) - 25, 493 - 92 * (ptrans.z / ACTIVE_RADIUS) - 25, GetColor(255, 255, 255), "%d", (int) ptrans.y);
 	}
 }
